cb09a71e7ebbd401118883dbc350fa965f4a6131
Vishwanatha Kamath Katapadi <namitha.289@gmail.com>
2022-07-17 17:04:34 -0400

    Added Descriptive quality comments

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,9 +1,12 @@
-const { Model, DataTypes } = require('sequelize');
+// import important parts of sequelize library
+const { Model, DataTypes } = require("sequelize");
+// import our database connection from config.js
+const sequelize = require("../config/connection.js");
 
-const sequelize = require('../config/connection.js');
-
+// Initialize Category model (table) by extending off Sequelize's Model class
 class Category extends Model {}
 
+// set up fields and rules for Category model
 Category.init(
   {
     // define columns
@@ -11,19 +14,19 @@ Category.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       autoIncrement: true,
-      primaryKey: true
+      primaryKey: true,
     },
     category_name: {
       type: DataTypes.STRING,
-      allowNull: false
-    }
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
